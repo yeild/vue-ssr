cf201f9b8089151bb9192cee759db93337e180ef
huanglei <798400626@qq.com>
2018-03-13 09:53:09 +0800

    client-side hydration

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,23 +1,9 @@
-/*
 import { createApp } from './app'
-import Vue from 'vue'
 
-Vue.mixin({
-  beforeRouteUpdate (to, from, next) {
-    const { asyncData } = this.$options
-    if (asyncData) {
-      asyncData({
-        store: this.$store,
-        route: to
-      }).then(next).catch(next)
-    } else {
-      next()
-    }
-  }
-})
+const { app, store } = createApp()
 
-const { app, router, store } = createApp()
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
-app.$mount('#app')*/
+
+app.$mount('#app')
